58653f6f5a7b43c432d299710604358acfedd61b
po.cheng <po.cheng@adlinktech.com>
2021-06-17 16:18:37 +0800

    ethernet: Enable Ethernet Phy with random mac address (Rev.A20)
    Signed-off-by: po.cheng <po.cheng@adlinktech.com>

--- board/adlink/lec-imx8mp/lec-imx8mp.c
+++ board/adlink/lec-imx8mp/lec-imx8mp.c
@@ -108,7 +108,7 @@ int ft_board_setup(void *blob, bd_t *bd)
 #endif
 
 #ifdef CONFIG_FEC_MXC
-#define FEC_RST_PAD IMX_GPIO_NR(4, 2)
+#define FEC_RST_PAD IMX_GPIO_NR(4, 4)
 static iomux_v3_cfg_t const fec1_rst_pads[] = {
 	MX8MP_PAD_SAI1_RXD0__GPIO4_IO02 | MUX_PAD_CTRL(NO_PAD_CTRL),
 };
@@ -130,7 +130,35 @@ static int setup_fec(void)
 	struct iomuxc_gpr_base_regs *gpr =
 		(struct iomuxc_gpr_base_regs *)IOMUXC_GPR_BASE_ADDR;
 
+	struct udevice *bus;
+	struct udevice *i2c_dev = NULL;
+	int ret;
+	unsigned char i2c_data[64];
+
 	setup_iomux_fec();
+
+	ret = uclass_get_device_by_seq(UCLASS_I2C, 0x5, &bus);
+	if (ret) {
+		printf("%s: Can't find bus\n", __func__);
+		return -EINVAL;
+	}
+
+	ret = dm_i2c_probe(bus, 0x70, 0, &i2c_dev);//GPIO expander address
+	if (ret) {
+		printf("%s: Can't find device id 0x70\n", __func__);
+		return -EINVAL;
+	}
+
+	//write 0x0 register 0
+	//Write 0xE register 0
+	dm_i2c_reg_write(i2c_dev, 0x0, 0);
+	dm_i2c_reg_write(i2c_dev, 0xe, 0);
+
+	ret = dm_i2c_read(i2c_dev, 0x0, i2c_data, 0x2A);
+	if (ret) {
+		printf("%s dm_i2c_read failed, err %d\n", __func__, ret);
+		return -EINVAL;
+	}
 
 	/* Enable RGMII TX clk output */
 	setbits_le32(&gpr->gpr[1], BIT(22));
